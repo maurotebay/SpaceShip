15c50e30d977a34ba460cc694e34af8d5eb7cd4e
Mauro Tebay <maurotebay@gmail.com>
2020-01-08 17:08:13 -0300

    Powerups and their respective messages  added

--- game.js
+++ game.js
@@ -17,6 +17,8 @@
     var gameOver = false;
     var score = 0;
     var enemies = [];
+    var powerups = [];
+    var messages = [];
 
     function init() {
         canvas = document.getElementById('canvas');
@@ -62,7 +64,17 @@
 
             // New Shot
             if (lastPress == KEY_SPACE) {
-                shots.push(new Rectangle(player.x + 3, player.y, 5, 5));
+                if (player.multiShots == 3) {
+                    shots.push(new Rectangle(player.x - 3, player.y + 2, 5, 5));
+                    shots.push(new Rectangle(player.x + 3, player.y, 5, 5));
+                    shots.push(new Rectangle(player.x + 9, player.y + 2, 5, 5));
+                }
+                else if (player.multiShots == 2) {
+                    shots.push(new Rectangle(player.x, player.y, 5, 5));
+                    shots.push(new Rectangle(player.x + 5, player.y, 5, 5));
+                }
+                else
+                    shots.push(new Rectangle(player.x + 3, player.y, 5, 5));
                 lastPress = null;
             }
 
@@ -77,7 +89,7 @@
                         sl--;
                     }
                 }
-                
+
                 //Move enemy
                 enemies[i].move('y', 5, 'down');
                 if (enemies[i].y > canvas.height) {
@@ -86,7 +98,7 @@
                 }
 
                 // Player Intersects Enemy
-                if (player.intersects(enemies[i]) && player.timer<1) {
+                if (player.intersects(enemies[i]) && player.timer < 1) {
                     player.healthLoss();
                     player.timer = 20;
                 }
@@ -101,12 +113,21 @@
                 }
 
                 //Kill enemies
-                if(enemies[i].health<1){
+                if (enemies[i].health < 1) {
+                    let r = random(20);
+                    if (r < 5) {
+                        if (r == 0) {   //type 1 is multi shot
+                            powerups.push(new PowerUp(enemies[i].x, enemies[i].y, 5, 5, 1));
+                        }
+                        else {  //type 0 is extra points
+                            powerups.push(new PowerUp(enemies[i].x, enemies[i].y, 5, 5, 0));
+                        }
+                    }
                     score++;
                     enemies.splice(i, 1);
                     enemies.push(new SpaceShip(random(canvas.width / 10) * 10, 0, 10, 10, 2));
                 }
-                
+
             }
 
             // Move Shots
@@ -118,13 +139,39 @@
                 }
             }
 
-            if(player.timer>0){
+            // Move PowerUps
+            for (var i = 0, l = powerups.length; i < l; i++) {
+                powerups[i].move('y', 5, 'down');
+                if (player.intersects(powerups[i])) {
+                    player.effect(powerups[i].type);
+                    powerups.splice(i--, 1);
+                    l--;
+                    continue;
+                }
+                // Powerup Outside Screen
+                if (powerups[i].y > canvas.height) {
+                    powerups.splice(i--, 1);
+                    l--;
+                    continue;
+                }
+            }
+
+            // Move Messages
+            for (var i = 0, l = messages.length; i < l; i++) {
+                messages[i].y += 2;
+                if (messages[i].y < 260) {
+                    messages.splice(i--, 1);
+                    l--;
+                }
+            }
+
+            if (player.timer > 0) {
                 player.timer--;
             }
-    
-            if(player.health < 1){
-                gameOver=true;
-                pause=true;
+
+            if (player.health < 1) {
+                gameOver = true;
+                pause = true;
             }
 
         }
@@ -141,39 +188,58 @@
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         ctx.fillStyle = '#0f0';
-        if(player.timer%2==0)
+        if (player.timer % 2 == 0)
             player.fill(ctx);
 
+        //print pause and game over
         ctx.fillStyle = '#fff';
         if (pause) {
             ctx.textAlign = 'center';
-            if(gameOver){
+            if (gameOver) {
                 ctx.fillText('GAME OVER', 100, 150);
             }
-            else{
+            else {
                 ctx.fillText('PAUSE', 100, 150);
             }
         }
         ctx.textAlign = 'left';
 
+        //print shots
         ctx.fillStyle = '#f00';
         for (var i = 0, l = shots.length; i < l; i++)
             shots[i].fill(ctx);
 
+        //print enemies
         for (var i = 0, l = enemies.length; i < l; i++) {
             ctx.fillStyle = '#00f';
             enemies[i].fill(ctx);
-
-            ctx.textAlign= 'center';
+            //print enemy's health in them
+            ctx.textAlign = 'center';
             ctx.fillStyle = '#f00';
-            ctx.fillText(enemies[i].health, enemies[i].x+5, enemies[i].y+8);
+            ctx.fillText(enemies[i].health, enemies[i].x + 5, enemies[i].y + 8);
         }
         ctx.textAlign = 'center';
 
-        ctx.fillStyle = '#fff';
-        ctx.fillText('Score: ' + score, 10, 10);
+        //print powerups
+        for (var i = 0, l = powerups.length; i < l; i++) {
+            if (powerups[i].type == 1)
+                ctx.fillStyle = '#f90';
+            else
+                ctx.fillStyle = '#cc6';
+            powerups[i].fill(ctx);
+        }
 
-        ctx.fillText('Health: ' + player.health, 150, 10);
+        //print powerup message
+        ctx.fillStyle = '#fff';
+        for (var i = 0, l = messages.length; i < l; i++)
+            ctx.fillText(messages[i].string, messages[i].x, messages[i].y);
+
+        //print score
+        ctx.fillStyle = '#fff';
+        ctx.fillText('Score: ' + score, 20, 10);
+
+        //print health
+        ctx.fillText('Health: ' + player.health, 160, 10);
     }
 
     document.addEventListener('keydown', function (evt) {
@@ -209,6 +275,7 @@
         gameOver = false;
         player.timer = 0;
         player.health = 3;
+        player.multiShots = 1;
     }
 
     class Rectangle {
@@ -219,13 +286,13 @@
             this.width = (width === undefined) ? 0 : width;
             this.height = (height === undefined) ? this.width : height;
         }
-    
+
         intersects(rect) {
-    
+
             if (rect === undefined) {
                 window.console.warn('Missing parameters');
             }
-    
+
             else {
                 return (this.x < rect.x + rect.width &&
                     this.x + this.width > rect.x &&
@@ -233,18 +300,18 @@
                     this.y + this.height > rect.y);
             }
         }
-    
+
         fill(ctx) {
-    
+
             if (ctx === undefined) {
                 window.console.warn('Missing parameters on function fill');
             }
-    
+
             else {
                 ctx.fillRect(this.x, this.y, this.width, this.height);
             }
         }
-    
+
         drawImage(ctx, img) {
             if (img === undefined) {
                 window.console.warn('Missing parameters on function drawImage');
@@ -256,7 +323,7 @@
                 }
             }
         }
-    
+
         move(axis, unit, direction) {
             if (axis === 'x') {
                 if (direction === 'left') {
@@ -275,7 +342,7 @@
                 }
             }
         }
-    
+
     }
 
     class SpaceShip extends Rectangle {
@@ -283,12 +350,46 @@
             super(x, y, width, height);
             this.health = (health === undefined) ? 1 : health;
             this.timer = 0;
+            this.multiShots = 1;
         }
-    
+
         healthLoss() {
             this.health--;
         }
-    
+
+        effect(typeOfPowerUp) {
+            if (typeOfPowerUp === 1) {
+                if (player.multiShots < 3) {
+                    player.multiShots++;
+                    messages.push(new Message('MULTI', player.x, player.y));
+                }
+                else {
+                    score += 5;
+                    messages.push(new Message('+5', player.x, player.y));
+                }
+            }
+            else {
+                score += 5;
+                messages.push(new Message('+5', player.x, player.y));
+            }
+        }
+
+    }
+
+    class PowerUp extends Rectangle {
+        constructor(x, y, width, height, type) {
+            super(x, y, width, height);
+            this.type = (type === undefined) ? 0 : type;
+        }
+
+    }
+
+    class Message {
+        constructor(string, x, y) {
+            this.string = (string === undefined) ? '?' : string;
+            this.x = (x === undefined) ? 0 : x;
+            this.y = (y === undefined) ? 0 : y;
+        }
     }
 
 })();
